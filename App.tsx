a3ff39c5983c317659e5f3c4b9f4942bdd3137b5
Raissa <raissa1068@gmail.com>
2021-09-21 23:30:09 -0400

    mema coisa corrigida o errinho la da virgula mkkkk

--- App.tsx
+++ App.tsx
@@ -17,14 +17,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    /*flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',*/
   },
   header: {
       height: 93,
-      backgroundColor: #2C4877,
+      marginTop: 40,
+      backgroundColor: '#2C4877',
       
   }
 });
