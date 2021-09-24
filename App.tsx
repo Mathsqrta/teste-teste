6f21d2230e9d70836d2a8e3288b478837dd6ed9d
Raissa <raissa1068@gmail.com>
2021-09-24 16:33:09 -0400

    fim aula 29

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
@@ -8,10 +8,19 @@ export default function App() {
       <View style={styles.header}>
 
         <Image source={require("./src/assets/seta.png")}></Image>
-        <Image source={require("./src/assets/ei-search.png")}></Image>
+
+        <View style={styles.inputTextView}>
+          <Image source={require("./src/assets/ei_search.png")}></Image>
+          <TextInput 
+          placeholder="Search"
+          placeholderTextColor="F5FFFF"
+          style={styles.textInput}
+          ></TextInput> 
+        </View>
+        
         <Image source={require("./src/assets/share.png")}></Image>
 
-        <Text>header</Text>
+        <Text>]</Text>
         
       </View>
       <Text>teste teste teste</Text>
@@ -22,16 +31,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    /*flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,
+    /*backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',*/
   },
   header: {
       minHeight: 93,
-      height: 93,
       marginTop: 23,
+      backgroundColor: '#4369B0',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+  },
+  inputTextView:{
+      flex: 1,
       backgroundColor: '#2C4877',
-      
+      height: 40,
+      width: 30,
+      marginHorizontal: 10,
+      alignItems: 'center',
+      borderRadius: 28,
+      flexDirection: 'row',
+  },
+  textInput:{
+    color:"#F5FFFF",
+    //maxWidth: 200,
+    flex: 1,
+    paddingRight: 10,
   }
 });
